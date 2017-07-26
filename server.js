9d1224f2478717be2d970c4294e92ff608583276
Ferlargut <ferlargut@gmail.com>
2017-07-25 22:26:38 -0500

    Area de trabajo

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use('/', express.static('public'));
+app.use("/static", express.static(path.join(__dirname, 'node_modules')));
+app.use("/static", express.static(path.join(__dirname, 'public/assets')));
+
+app.listen(3000, function () {
+    console.log("hola 1388");
+});
---
+++ src/assets/js/app.js
@@ -0,0 +1,1 @@
+console.log($("h").text("hola3"));
